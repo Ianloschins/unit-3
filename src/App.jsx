4735bd779253bb9ed50a735dab23a33b16d650d0
Ian <ian.loschinskey@gmmail.com>
2023-06-29 15:56:00 -0400

    Adding CSS a little touch.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import {puppyList} from './data.js'
 import './App.css'
 
+
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
@@ -21,10 +22,10 @@ function App() {
         );
       })}
       {featPupId && (
-      <div>
+      <div className="featured-pup">
       <h2>{featuredPup.name}</h2>
       <ul>
-        <li>Age: {featuredPup.age}</li>
+        <li>Age: {featuredPup.age}<br></br></li>
         <li>Email: {featuredPup.email}</li>
       </ul>
     </div>
